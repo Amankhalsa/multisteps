4ba36326d8946f01769aa030cc02b748a46089ef
sunil43091 <sk3645797@gmail.com>
2022-04-26 15:13:53 +0530

    updated form

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {	
 	
 	// Random Alert shown for the fun of it
-	function randomAlert() {
-		var min = 5,
-			max = 20;
-		var rand = Math.floor(Math.random() * (max - min + 1) + min); //Generate Random number between 5 - 20
-		// post time in a <span> tag in the Alert
-		$("#time").html('Next alert in ' + rand + ' seconds');
-		$('#timed-alert').fadeIn(500).delay(3000).fadeOut(500);
-		setTimeout(randomAlert, rand * 1000);
-	};
-	randomAlert();
+	// function randomAlert() {
+	// 	var min = 5,
+	// 		max = 20;
+	// 	var rand = Math.floor(Math.random() * (max - min + 1) + min); //Generate Random number between 5 - 20
+	// 	// post time in a <span> tag in the Alert
+	// 	$("#time").html('Next alert in ' + rand + ' seconds');
+	// 	$('#timed-alert').fadeIn(500).delay(3000).fadeOut(500);
+	// 	setTimeout(randomAlert, rand * 1000);
+	// };
+	// randomAlert();
 });
 
 $('.btn').click(function(event) {
